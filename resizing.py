6a705c9c6a43c64f005acbecf645d2a0a6e19f0a
vanilla028 <145339245+vanilla028@users.noreply.github.com>
2023-09-30 23:35:19 +0900

    Delete resizing.py

--- resizing.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import os
-
-
-img = Image.open("C:/Users/happysis/Documents/카카오톡 받은 파일/b.png")
-
-img_resize = img.resize((128, 128))
-img_resize.save('생각하는 로댕.png')
-print(img_resize.size)
