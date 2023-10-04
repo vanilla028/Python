410f3e5a851432cbf21e3b801c9cd195ed72ddcc
vanilla028 <145339245+vanilla028@users.noreply.github.com>
2023-10-05 06:21:17 +0900

    Update and rename numgame.py to Numgame.py

--- Numgame.py
+++ Numgame.py
@@ -1,7 +1,7 @@
 import random
 random_number = random.randint(1, 100)
 
-#print(random_number)
+# print(random_number)
 game_count = 1
 
 while True:
@@ -12,6 +12,6 @@ while True:
         print("up!")
     else:
         print(f"Congratulations! {game_count}번 만에 맞추셨습니다")
-#f == format 함수. 포매팅시킨다(문자열포매팅) 
+# f == format 함수. 포매팅시킨다(문자열포매팅) 
         break
     game_count = game_count + 1
