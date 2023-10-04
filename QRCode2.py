4d9c2cd38871e6beb3cc83b85f06b01329bf1f81
vanilla028 <145339245+vanilla028@users.noreply.github.com>
2023-10-05 06:17:01 +0900

    Update QRCode2.py

--- QRCode2.py
+++ QRCode2.py
@@ -1,6 +1,6 @@
 import qrcode
 
-#with 키워드를 사용하여 파일을 열면, 파일을 사용하지 않을 때 자동으로 close()를 호출하여 닫아준다.
+# with 키워드를 사용하여 파일을 열면, 파일을 사용하지 않을 때 자동으로 close()를 호출하여 닫아준다.
 with open('site_list.txt', 'rt', encoding='UTF8') as f:
   read_lines = f.readlines()
 
