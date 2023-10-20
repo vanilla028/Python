b8626d45aa2e7b7b8485956fa8d1312fff5fccc4
vanilla028 <alison28@daum.net>
2023-10-20 23:39:17 +0900

    stage for python

--- practice/stage1.py
+++ practice/stage1.py
@@ -1,2 +1,16 @@
 # 1 Hello World!를 출력하시오.
 print("Hello, world!")
+
+# 2 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+a, b = map(int, input().split()) # 공백 문자를 기준으로 문자열 분할
+print(a+b)
+
+# 3 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+a, b = map(int, input().split())
+if a > 0:
+    if b <10:
+        print(a-b)
+    else:
+        print("error!")
+
+# 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
