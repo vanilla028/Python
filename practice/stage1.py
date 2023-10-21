d0a45b2b867cc24280e0095421d8dc434645d930
vanilla028 <alison28@daum.net>
2023-10-21 10:07:08 +0900

    stage for python

--- practice/stage1.py
+++ practice/stage1.py
@@ -6,11 +6,36 @@ a, b = map(int, input().split()) # 공백 문자를 기준으로 문자열 분�
 print(a+b)
 
 # 3 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+# 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
 a, b = map(int, input().split())
 if a > 0:
     if b <10:
         print(a-b)
     else:
-        print("error!")
+        print("Error!")
 
 # 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+# 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+a, b = map(int, input().split())
+if a > 0:
+    if b <10:
+        print(a * b)
+else:
+    print("Error!")
+
+# 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+# 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+a, b = map(int, input().split())
+if a > 0:
+    if b <10:
+        print(a/b)
+else:
+    print("Error!")
+
+# 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+a, b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
