ba8ad218ca2821af5fc0f602d61b393b08d2466c
vanilla028 <alison28@daum.net>
2023-10-22 01:27:17 +0900

    time

--- practice/stage7.py
+++ practice/stage7.py
@@ -1,8 +1,8 @@
 # 45분 일찍 알람 설정하기
-h, m = map(int, input().split())
-if m < 45:
-    m = m + 15
-    print(h - 1,":", m)
-else:
-    m = 45 - m
-    print(h, ":", m)
+
+h, m = map(int, input("알람 시간 설정: ").split())
+total = h * 60 + m - 45
+h = total // 60
+m = total % 60
+
+print(f"알람 설정 시간: {h}:{m}")
