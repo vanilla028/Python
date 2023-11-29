7dcbc75482bf36476c3118762bbd22db160c2167
vanilla028 <alison28@daum.net>
2023-11-29 17:12:10 +0900

    library2

---
+++ Basic/library2.py
@@ -0,0 +1,55 @@
+import itertools
+
+# 순열
+
+# 조합
+
+# 긴 객체에 맞춰 fillvalue 채우기
+
+# 객체를 하나의 값으로 줄이기
+
+# 최댓값 구하기
+
+# 다양한 기준으로 정렬하기
+
+
+import shutil
+# 파일을 복사(copy)하거나 이동(move)하기
+
+import glob
+# 디렉터리에 있는 파일들을 리스트로 만들기
+
+import pickle
+# 객체를 파일에 저장하고 로드하기
+
+import os
+# 환경 변수, 디렉토리, 파일 등의 OS 자원을 제어하는 모듈
+
+# 환경 변숫값 확인하기
+
+# 디렉토리 위치 변경하기
+
+# 디렉터리 위치 반환하기
+
+# 시스템 명령어 호출하기
+
+# 시스템 명령어 반환하기
+
+# 여러 개의 파일을 zip 형식으로 병합 또는 해제하기
+
+import time
+# 스레딩
+
+import tempfile
+# 파일을 임시로 만들어서 사용할 때 유용한 모듈
+
+# traceback
+
+import json
+# JSON 파일 읽고 쓰기
+
+import urllib.request
+# URL 읽고 분석할 때 사용하는 모듈
+
+
+
